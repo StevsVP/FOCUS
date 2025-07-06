a5e0b3e29d42c9401613dd1476102e56358f4c14
Esteban <info.estebanvp@gmail.com>
2025-07-06 01:54:57 -0400

    PopUp boton Mas Detalles, Agregar al carrito diciendo Proximamente

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,15 @@ function mostrarHoraActual() {
 // Iniciar el reloj
 setInterval(mostrarHoraActual, 1000);
 mostrarHoraActual();
+
+
+//Modal Productos
+function mostrarDetalle(nombre, descripcion, categoria, precio, imagen) {
+  document.getElementById('detalleModalLabel').textContent = nombre;
+  document.getElementById('detalle-desc').textContent = descripcion;
+  document.getElementById('detalle-cat').textContent = categoria;
+  document.getElementById('detalle-precio').textContent = precio;
+  const img = document.getElementById('detalle-img');
+  img.src = imagen;
+  img.alt = nombre;
+}
